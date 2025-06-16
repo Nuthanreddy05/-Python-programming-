b00c6e3704bb673cfe455028d87d2bed53f01ebc
Nuthanreddy05 <144426733+Nuthanreddy05@users.noreply.github.com>
2025-06-16 18:13:18 -0500

    Add files via upload

---
+++ feature_selection (1).py
@@ -0,0 +1,22 @@
+
+"""The five columns that i like to remove from the data are:['LowUserPrice','ReleaseYear','LowNetPrice','Order','MarketingType']
+
+1)LowUserPrice:The lowest user price represents the minimum cost at which a customer has purchased the product, indicating the lowest price point for consumers. so, the lowuserprice can be igonred.
+
+2)Order:The order number is simply a sequential or index number.It doesn't contain any information related to the data. Therefore, the order column can be safely ignored without affecting the integrity of the data.
+
+3)ReleaseYear:The release year is the column in which it provides information about the product in which  year the product has been manufactured so which can be ignored at the process of creating the object class of the product.
+
+4)LowNetPrice:The "Low net price" refers to a reduced total price after deductions or adjustments have been applied, typically resulting in a cost-saving for the buyer. so, it can be ignored.
+
+5)MarketingType:I have removed the marketing type coloumn beacause it's just the coloumn which represents how we market the product which can be ignored, which can be considered independently 
+ 
+The two columns that are very important are:
+
+1)SKU Number:sku number is the unique identifier for each product, identifying individual products.
+it important column in the data for the identifying the each product with unqiue number for each product.
+ 
+2) StrengthFactor:This attribute offers valuable insights into the product's quality and overall appeal, influencing decisions about its ongoing market availability."""
+
+
+
